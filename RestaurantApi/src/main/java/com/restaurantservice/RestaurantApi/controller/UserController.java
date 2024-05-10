e9536388bc545dbbdbde09403dee6b9b06c96c72
quoc-tuan <nvtuanq123@gmail.com>
2024-05-09 21:05:16 -0700

    Fix: Add Jwt generation when sign-up & login

--- RestaurantApi/src/main/java/com/restaurantservice/RestaurantApi/controller/UserController.java
+++ RestaurantApi/src/main/java/com/restaurantservice/RestaurantApi/controller/UserController.java
@@ -3,11 +3,18 @@ package com.restaurantservice.RestaurantApi.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.restaurantservice.RestaurantApi.dto.JwtResponseDto;
 import com.restaurantservice.RestaurantApi.dto.UserDto;
+import com.restaurantservice.RestaurantApi.service.JwtService;
 import com.restaurantservice.RestaurantApi.service.UserService;
 
 @RestController
@@ -16,9 +23,34 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private JwtService jwtService;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public ResponseEntity<?> login(@RequestBody UserDto userDto) {
+		Authentication authentication = authenticationManager
+				.authenticate(new UsernamePasswordAuthenticationToken(userDto.getUserName(), userDto.getPassword()));
+
+		if (authentication.isAuthenticated()) {
+			JwtResponseDto token = JwtResponseDto.builder().accessToken(jwtService.generateToken(userDto.getUserName()))
+					.build();
+			return ResponseEntity.ok().body(token);
+		}
+		return ResponseEntity.badRequest().body("Login failed!");
+	}
+
+	
 	@PostMapping("/v1/sign-up")
 	public ResponseEntity<?> store(@RequestBody UserDto userDto) {
+		if (userService.findByUserName(userDto.getUserName()).isPresent()) {
+			return ResponseEntity.badRequest().body("User name exists!");
+		}
 		userService.store(userDto);
-		return ResponseEntity.status(HttpStatus.CREATED).body("Create user success!");
+		JwtResponseDto token = JwtResponseDto.builder().accessToken(jwtService.generateToken(userDto.getUserName()))
+				.build();
+		return ResponseEntity.status(HttpStatus.CREATED).body(token);
 	}
 }
---
+++ RestaurantApi/src/main/java/com/restaurantservice/RestaurantApi/dto/JwtResponseDto.java
@@ -0,0 +1,15 @@
+package com.restaurantservice.RestaurantApi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JwtResponseDto {
+
+	private String accessToken;
+}
