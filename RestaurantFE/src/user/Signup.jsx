d7ef94628c921d24bc3886b12c485591e9258e60
quoc-tuan <nvtuanq123@gmail.com>
2024-05-09 20:21:15 -0700

    Feat: Add sign-up page

--- RestaurantFE/src/user/Signup.jsx
+++ RestaurantFE/src/user/Signup.jsx
@@ -1,7 +1,36 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import ApiClient from "../assets/js/ApiClient";
 
 const Signup = () => {
+  const userNameRef = useRef();
+  const passwordRef = useRef();
+  const fullNameRef = useRef();
+  const birthDayRef = useRef();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const userName = userNameRef.current.value;
+    const password = passwordRef.current.value;
+    const fullName = fullNameRef.current.value;
+    const birthDay = birthDayRef.current.value;
+    const formData = {
+      userName: userName,
+      password: password,
+      fullName: fullName,
+      birthDayRef: birthDayRef.toString(),
+    };
+    const resp = await ApiClient.post("/v1/sign-up", formData).then((e) => e);
+
+    if (resp.status == 201) {
+      localStorage.setItem("token", resp.data.accessToken);
+      navigate("/home");
+    } else {
+    }
+  };
+
   return (
     <div className="container">
       <div className="row justify-content-center">
@@ -15,19 +44,20 @@ const Signup = () => {
                     <div className="text-center">
                       <h1 className="h4 text-gray-900 mb-4">Sign up user!</h1>
                     </div>
-                    <form className="user">
+                    <form className="user" onSubmit={handleSubmit}>
                       <div className="form-group">
                         <input
-                          type="email"
+                          ref={userNameRef}
+                          type="text"
                           className="form-control form-control-user"
-                          id="email"
-                          aria-describedby="emailHelp"
-                          placeholder="Enter Email Address..."
+                          id="userName"
+                          placeholder="Enter User name"
                         />
                       </div>
                       <div className="form-group">
                         <input
-                          type="password"
+                          ref={fullNameRef}
+                          type="text"
                           className="form-control form-control-user"
                           id="fullName"
                           placeholder="Enter your full name"
@@ -35,6 +65,7 @@ const Signup = () => {
                       </div>
                       <div className="form-group">
                         <input
+                          ref={passwordRef}
                           type="password"
                           className="form-control form-control-user"
                           id="pasword"
@@ -43,17 +74,18 @@ const Signup = () => {
                       </div>
                       <div className="form-group">
                         <input
+                          ref={birthDayRef}
                           type="date"
                           className="form-control form-control-user"
                           id="birthDate"
                         />
                       </div>
-                      <a
-                        href="index.html"
+                      <button
+                        type="submit"
                         className="btn btn-primary btn-user btn-block"
                       >
-                        Login
-                      </a>
+                        Register
+                      </button>
                       <hr />
                     </form>
                     <hr />
