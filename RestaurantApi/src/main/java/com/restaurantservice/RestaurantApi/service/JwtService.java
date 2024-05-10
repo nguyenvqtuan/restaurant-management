1ce3bd498ee3ab4d4c54dc207aef412142f8a90d
quoc-tuan <nvtuanq123@gmail.com>
2024-05-09 19:01:10 -0700

    Feat: Add Jwt config

---
+++ RestaurantApi/src/main/java/com/restaurantservice/RestaurantApi/service/JwtService.java
@@ -0,0 +1,18 @@
+package com.restaurantservice.RestaurantApi.service;
+
+import java.util.Date;
+import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+import io.jsonwebtoken.Claims;
+
+public interface JwtService {
+
+	String extractUsername(String token);
+	Date extractExpiration(String token);
+	<T> T extractClaim(String token, Function<Claims, T> claimsResolver);
+	
+	Boolean validateToken(String token, UserDetails userDetails);
+	String generateToken(String username);
+}
