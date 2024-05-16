9d4653031857c535115ff0a491a53f06696c27d8
quoc-tuan <nvtuanq123@gmail.com>
2024-05-15 20:08:01 -0700

    Feat: Add inventory detail entity

---
+++ RestaurantApi/src/main/java/com/restaurantservice/RestaurantApi/dto/InventoryDetailDto.java
@@ -0,0 +1,29 @@
+package com.restaurantservice.RestaurantApi.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class InventoryDetailDto {
+
+	private int id;
+	private String name;
+	
+	// 0: char, 1: table
+	private byte type;
+	
+	// 0: available, 1: using
+	private boolean status;
+	
+	private int inventoryId;
+	
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+}
---
+++ RestaurantApi/src/main/java/com/restaurantservice/RestaurantApi/entity/InventoryDetailEntity.java
@@ -0,0 +1,39 @@
+package com.restaurantservice.RestaurantApi.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="inventory_detail")
+@Getter
+@Setter
+public class InventoryDetailEntity {
+
+	@Id
+	@GeneratedValue
+	private int id;
+	private String name;
+	
+	// 0: char, 1: table
+	private byte type;
+	
+	// 0: available, 1: using
+	private boolean status;
+	
+	private int inventoryId;
+	
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+	
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+}
