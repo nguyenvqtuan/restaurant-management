cf11bcd0da5cff6f2c410a984fd289bd78d99ae9
quoc-tuan <nvtuanq123@gmail.com>
2024-05-21 17:38:56 -0700

    Feat: Add CRUD view inventory detail

---
+++ RestaurantFE/src/component/inventory/FormInventoryDetail.jsx
@@ -0,0 +1,102 @@
+import React, { useRef, useState, useEffect } from "react";
+import { Button, Modal } from "react-bootstrap";
+import { success, error } from "../../assets/js/SweetCustom";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+
+const FormInventoryDetail = ({ show, handleClose, inventoryId, id }) => {
+  const axiosPrivate = useAxiosPrivate();
+  const [inventoryDetail, setInventoryDetail] = useState();
+  const nameRef = useRef();
+  const statusRef = useRef();
+
+  useEffect(() => {
+    getInventoryDetail();
+  }, [id]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const name = nameRef.current.value;
+    const status = statusRef.current.value;
+    const formData = {
+      id,
+      name,
+      status,
+    };
+    const resp = await axiosPrivate
+      .post(`/inventory/${inventoryId}/detail`, formData)
+      .catch((err) => {
+        error(err.repsonse.data.message);
+      });
+
+    handleClose();
+    if (resp?.status == 200) {
+      success(resp.data);
+    }
+  };
+
+  const getInventoryDetail = async () => {
+    const resp = await axiosPrivate
+      .get(`/inventory/${inventoryDetail}/detail/${id}`)
+      .catch((err) => {
+        error(err.response.data.message);
+      });
+    if (resp?.data != null) setInventoryDetail(resp.data);
+  };
+
+  const handleInputChange = (e) => {
+    const { value, name } = e.target;
+    setInventoryDetail(value);
+  };
+
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header>
+        <Modal.Title>Inventory Detail</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <form className="user" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <input type="hidden" value={id} />
+          </div>
+          <div className="form-group">
+            <input
+              value={inventoryDetail?.name}
+              name="name"
+              onChange={handleInputChange}
+              ref={nameRef}
+              type="text"
+              className="form-control form-control-user"
+              id="name"
+              placeholder="Enter name"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              value={inventoryDetail?.status}
+              name="status"
+              onChange={handleInputChange}
+              ref={statusRef}
+              type="text"
+              className="form-control form-control-user"
+              id="status"
+              placeholder="Enter status"
+            />
+          </div>
+
+          <hr />
+        </form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={handleSubmit}>
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default FormInventoryDetail;
--- RestaurantFE/src/component/inventory/ManageInventoryDetail.jsx
+++ RestaurantFE/src/component/inventory/ManageInventoryDetail.jsx
@@ -1,7 +1,133 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Slidebar from "../common/Slidebar";
+import Topbar from "../common/Topbar";
+import Footer from "../common/Footer";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import { success, error } from "../../assets/js/SweetCustom";
+import { Button } from "react-bootstrap";
+import FormInventoryType from "./FormInventoryType";
 
-const ManageInventoryDetail = () => {
-  return <div>ManageInventoryDetail</div>;
+const ManageInventoryDetail = (inventoryId) => {
+  const [id, setId] = useState(0);
+  const [inventoryDetail, setInventoryDetail] = useState();
+  const axiosPrivate = useAxiosPrivate();
+  useEffect(() => {
+    fetchInventoryType();
+  }, []);
+
+  const fetchInventoryDetail = async () => {
+    const resp = await axiosPrivate.get(`/inventory/${inventoryId}/detail`);
+    setInventoryType(resp.data);
+  };
+
+  const handleDelete = async (id) => {
+    const resp = await axiosPrivate
+      .delete(`/inventory/${inventoryId}/detail/${id}`)
+      .catch((err) => {
+        error("Inventory Detail", err.response.data.message);
+      });
+
+    if (resp?.status == 200) {
+      success("Inventory Detail", resp.data);
+      fetchInventoryDetail();
+    }
+  };
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+
+  const handleShowForm = (id) => {
+    setShow(true);
+    setId(id);
+  };
+
+  return (
+    <div id="wrapper">
+      <Slidebar />
+      <div id="content-wrapper" className="d-flex flex-column">
+        <div id="content">
+          <Topbar />
+          <div className="card shadow mb-4">
+            <div className="card-header py-3">
+              <h6 className="m-0 font-weight-bold text-primary d-inline">
+                Inventory type table
+              </h6>
+
+              <Button
+                variant="primary"
+                className="float-right"
+                onClick={() => handleShowForm(0)}
+              >
+                Create new inventory detail
+              </Button>
+              <FormInventoryDetail
+                show={show}
+                handleClose={handleClose}
+                id={id}
+              />
+            </div>
+            <div className="card-body">
+              <div className="table-responsive">
+                <table
+                  id="dataTable"
+                  className="table table-bordered"
+                  width="100%"
+                  cellSpacing="0"
+                >
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Type</th>
+                      <th>Status</th>
+                      <th>Created at</th>
+                      <th>Updated at</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tfoot>
+                    <tr>
+                      <th>Name</th>
+                      <th>Type</th>
+                      <th>Status</th>
+                      <th>Created at</th>
+                      <th>Updated at</th>
+                      <th>Action</th>
+                    </tr>
+                  </tfoot>
+                  <tbody>
+                    {inventoryDetail?.map((item) => (
+                      <tr key={item.id}>
+                        <td>{item.name}</td>
+                        <td>{item.type}</td>
+                        <td>{item.status}</td>
+                        <td>{item.createdAt}</td>
+                        <td>{item.updatedAt}</td>
+                        <td>
+                          <Button
+                            variant="primary"
+                            onClick={() => handleShowForm(item.id)}
+                          >
+                            Update
+                          </Button>
+                          <button
+                            className="btn btn-danger btn-circle"
+                            onClick={() => handleDelete(item.id)}
+                          >
+                            <i className="fas fa-trash"></i>
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default ManageInventoryDetail;
