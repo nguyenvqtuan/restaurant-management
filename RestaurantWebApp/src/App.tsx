18e05da0cc5505640945f117e75725de4d21cc53
quoctuan <nvtuanq123@gmail.com>
2024-06-09 11:38:15 +0700

    feature: config alias path with typescript + nodejs

--- RestaurantWebApp/src/App.tsx
+++ RestaurantWebApp/src/App.tsx
@@ -1,11 +1,21 @@
-import './App.css'
+// import React, { useEffect, useState, Suspense } from 'react';
+// import { Router as RouterHistory } from "react-router-dom";
+// import { Button, Result } from 'antd';
+// import { Provider } from "react-redux";
+// import Router from "@router";
+// import Login from "@/pages/Login";
 
+import { Provider } from "react-redux";
+// import { Layout } from "antd";
+import Router from "./router";
+// import AuthRouter from "@/router/AuthRouter";
+import store from "@/redux/store";
 
-function App() {
-
+const App = () =>{
   return (
-    <>
-    </>
+    <Provider store={store}>
+        <Router />
+    </Provider>
   )
 }
 
