7f2c2d7baa5c4880878ddb711aae9514ac740fd3
quoctuan <nvtuanq123@gmail.com>
2024-06-09 15:53:35 +0700

    Feature: Config check login page

--- RestaurantWebApp/src/router/AppRouter.tsx
+++ RestaurantWebApp/src/router/AppRouter.tsx
@@ -1,16 +1,12 @@
 import React, { lazy} from 'react'
 import { Route } from 'react-router-dom'
 
-const Login = lazy(() => 
-  import(/*webpackChunkName:'LoginPage'*/ "@/pages/Login")
-)
-
 const AuthRouter = () => {
   // const location = useLocation();
 
   return (
     // <Route action key={location.pathname}>
-      <> need to login</>
+      <> login success </>
     // </Route>
   )
 }
--- RestaurantWebApp/src/router/index.tsx
+++ RestaurantWebApp/src/router/index.tsx
@@ -1,23 +1,38 @@
 import { useEffect } from 'react'
 import { Layout } from 'antd'
+
+import { useAppDispatch, useAppSelector } from '@/redux/redux-hook'
+import { selectUserInfo, setUserInfo } from '@/redux/slicers/userSlice'
+import AppRouter from "./AppRouter"
 import AuthRouter from "./AuthRouter"
-import {  useAppSelector } from '@/redux/redux-hook'
-import { selectUserInfo } from '@/redux/slicers/userSlice'
 
 const Router = () => {
   // const dispatch = useAppDispatch();
   const userInfo = useAppSelector(selectUserInfo);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     console.log(userInfo)
-
+    const result = {
+      username: "admin",
+      password: "123",
+      isLoggedIn: false,
+    }
+    dispatch(setUserInfo(result))
   }, [])
  
-  return (
-    <Layout>
-      <AuthRouter />
-    </Layout>
-  )
+  if (userInfo?.isLoggedIn) 
+    return (
+      <Layout>
+        <AppRouter />
+      </Layout>
+    )
+  else
+    return (
+      <Layout>
+        <AuthRouter />
+      </Layout>
+    )
 }
 
 export default Router
