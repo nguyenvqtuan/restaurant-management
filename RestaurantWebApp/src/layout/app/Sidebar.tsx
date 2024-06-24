2c8e4f7d48fb0aa40eb9bae2a6287e8ea3d08551
quoctuan <nvtuanq123@gmail.com>
2024-06-25 06:42:10 +0700

    feature: Add sidebar

--- RestaurantWebApp/src/layout/app/Sidebar.tsx
+++ RestaurantWebApp/src/layout/app/Sidebar.tsx
@@ -1,37 +1,82 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-
 import {
+  CBadge,
   CCloseButton,
+  CNavGroup,
+  CNavItem,
+  CNavTitle,
   CSidebar,
   CSidebarBrand,
   CSidebarFooter,
   CSidebarHeader,
+  CSidebarNav,
   CSidebarToggler,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
+import { cilCloudDownload, cilLayers, cilPuzzle, cilSpeedometer } from '@coreui/icons'
 
 const Sidebar = () => {
-  return (<CSidebar
-    className="border-end"
-    colorScheme="dark"
-    position="fixed"
-  >
-    <CSidebarHeader className="border-bottom">
-      <CSidebarBrand>
-        <CIcon customClassName="sidebar-brand-full" />
-        <CIcon customClassName="sidebar-brand-narrow" />
-      </CSidebarBrand>
-      <CCloseButton
-        className="d-lg-none"
-        dark
-      />
-    </CSidebarHeader>
-    <CSidebarFooter className="border-top d-none d-lg-flex">
-      <CSidebarToggler
-      />
-    </CSidebarFooter>
-  </CSidebar>
+  return (
+    <CSidebar
+      className="border-end"
+      colorScheme="dark"
+      position="fixed"
+    >
+      <CSidebarHeader className="border-bottom">
+        <CSidebarBrand>
+          <CIcon customClassName="sidebar-brand-full" />
+          <CIcon customClassName="sidebar-brand-narrow" />
+        </CSidebarBrand>
+        <CCloseButton
+          className="d-lg-none"
+          dark
+        />
+      </CSidebarHeader>
+      <CSidebarNav>
+        <CNavTitle>Restaurant admin</CNavTitle>
+        <CNavItem href="#">
+          <CIcon customClassName="nav-icon" icon={cilSpeedometer} />
+          Menu
+        </CNavItem>
+        <CNavItem href="#">
+          <CIcon customClassName="nav-icon" icon={cilSpeedometer} />
+          Inventory
+          <CBadge color="primary ms-auto">
+            NEW
+          </CBadge>
+        </CNavItem>
+        <CNavGroup
+          toggler={
+            <>
+              <CIcon customClassName="nav-icon" icon={cilPuzzle} /> Order
+            </>
+          }
+        >
+          <CNavItem href="#">
+            <span className="nav-icon">
+              <span className="nav-icon-bullet"></span>
+            </span>
+            List Order
+          </CNavItem>
+          <CNavItem href="#">
+            <span className="nav-icon">
+              <span className="nav-icon-bullet"></span>
+            </span>
+            List order detail
+          </CNavItem>
+        </CNavGroup>
+        <CNavItem href="https://coreui.io">
+          <CIcon customClassName="nav-icon" icon={cilCloudDownload} />
+          Download CoreUI
+        </CNavItem>
+        <CNavItem href="https://coreui.io/pro/">
+          <CIcon customClassName="nav-icon" icon={cilLayers} />
+          Try CoreUI PRO
+        </CNavItem>
+      </CSidebarNav>
+      <CSidebarFooter className="border-top d-none d-lg-flex">
+        <CSidebarToggler />
+      </CSidebarFooter>
+    </CSidebar>
   )
 }
 
