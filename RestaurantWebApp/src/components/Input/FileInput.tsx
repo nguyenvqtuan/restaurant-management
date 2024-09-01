5bf14ee8574886f619ac014460499ef305bbd57d
quoctuan <nvtuanq123@gmail.com>
2024-09-01 17:03:23 +0700

    [feature/frontend/common] Add common FIle input preview image

---
+++ RestaurantWebApp/src/components/Input/FileInput.tsx
@@ -0,0 +1,49 @@
+import { CFormInput, CFormLabel } from "@coreui/react"
+import { useState } from "react"
+
+interface InputFileProps {
+  onChange?: any
+  value?: string | null
+}
+
+const FileInput = (props: InputFileProps) => {
+  const { onChange, value } = props
+  const [imagePreview, setImagePreview] = useState(value || null);
+
+  const handleChange = (event: any) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview(reader.result);
+        onChange(file); // Pass the file up to the parent form
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <>
+      <CFormLabel htmlFor="inputName">Name</CFormLabel>
+      <CFormInput
+        type="file"
+        accept="image/*"
+        onChange={handleChange}
+      />
+      <div className="mt-2 border" style={{ height: '150px', width: '200px' }}>
+        {
+          imagePreview &&
+          <img
+            src={imagePreview}
+            alt="Preview"
+            style={{ width: '200px', height: '150px' }}
+          />
+        }
+      </div>
+    </>
+
+  )
+
+}
+
+export default FileInput
